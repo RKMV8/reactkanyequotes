7cd5d5983d56cfbb35709ca6e729bddc6020107c
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-21 15:27:45 +0100

    Added quotation marks

--- src/QuoteGenerator.js
+++ src/QuoteGenerator.js
@@ -11,7 +11,7 @@ import React from 'react'
     }
 
       return <div className="QuoteSection">
-      <p id="actualText">{quote}</p>
+      <p id="actualText">"{quote}"</p>
       <img src="https://svgshare.com/i/fLF.svg" alt="kanye"></img>
       <button onClick={fetchText}>Quote me! ⚡</button>
       </div>
