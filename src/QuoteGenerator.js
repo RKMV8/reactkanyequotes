26ed5263bf585d1d15ee24b39bab3d88138d06a4
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-21 15:17:47 +0100

    Refactored QuoteGenerator

--- src/QuoteGenerator.js
+++ src/QuoteGenerator.js
@@ -1,11 +1,5 @@
 import React from 'react'
 
-// async function fetchText() {
-//     let response = await fetch('https://api.kanye.rest');
-//     let data = await response.json();
-//     document.getElementById('actualText').innerHTML = "\""+data.quote+"\"";;
-//   }
-
   function QuoteGenerator(){
 
     const [quote, setQuote] = React.useState("Click the button below to generate a quote from Kanye!");
