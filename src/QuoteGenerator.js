b574446862287f2f0de75677b2364746628e6e6f
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-21 15:15:54 +0100

    Incorporated useState

--- src/QuoteGenerator.js
+++ src/QuoteGenerator.js
@@ -1,14 +1,23 @@
 import React from 'react'
 
-async function fetchText() {
-    let response = await fetch('https://api.kanye.rest');
-    let data = await response.json();
-    document.getElementById('actualText').innerHTML = "\""+data.quote+"\"";;
-  }
+// async function fetchText() {
+//     let response = await fetch('https://api.kanye.rest');
+//     let data = await response.json();
+//     document.getElementById('actualText').innerHTML = "\""+data.quote+"\"";;
+//   }
 
   function QuoteGenerator(){
+
+    const [quote, setQuote] = React.useState("Click the button below to generate a quote from Kanye!");
+
+    const fetchText = async () => {
+      let response = await fetch('https://api.kanye.rest/');
+      let data = await response.json();
+      setQuote(data.quote);
+    }
+
       return <div className="QuoteSection">
-      <p id="actualText">Click the button below to generate a quote from Kanye!</p>
+      <p id="actualText">{quote}</p>
       <img src="https://svgshare.com/i/fLF.svg" alt="kanye"></img>
       <button onClick={fetchText}>Quote me! ⚡</button>
       </div>
